6293a4815e3ed97058e50e6989c3734a0b824393
shahn03 <anseunghyok@gmail.com>
2022-01-18 13:30:27 +0900

    commit 2022.01.18

---
+++ src/test/java/com/shahn03/spring/web/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.shahn03.spring.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+//TDD 테스트를 위해서 main > java 와 동일한 경로에 파일을 생성하고
+//테스트를 위한 HelloController.class 의 객체를 가져와 주입한다.
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void return_hello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
